7577309b19e1a4db29d0c1bb9cfff6f382a53fd5
Abigail Oluyede <nife.oluyede@gmail.com>
2024-09-12 17:39:55 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,144 @@
+const product = document.querySelector(".product");
+
+const products = document.querySelector(".products");
+
+const productData = [
+  {
+    image: {
+      thumbnail: "./assets/images/image-waffle-thumbnail.jpg",
+      mobile: "./assets/images/image-waffle-mobile.jpg",
+      tablet: "./assets/images/image-waffle-tablet.jpg",
+      desktop: "./assets/images/image-waffle-desktop.jpg",
+    },
+    name: "Waffle with Berries",
+    category: "Waffle",
+    price: 6.5,
+  },
+  {
+    image: {
+      thumbnail: "./assets/images/image-creme-brulee-thumbnail.jpg",
+      mobile: "./assets/images/image-creme-brulee-mobile.jpg",
+      tablet: "./assets/images/image-creme-brulee-tablet.jpg",
+      desktop: "./assets/images/image-creme-brulee-desktop.jpg",
+    },
+    name: "Vanilla Bean Crème Brûlée",
+    category: "Crème Brûlée",
+    price: 7.0,
+  },
+  {
+    image: {
+      thumbnail: "./assets/images/image-macaron-thumbnail.jpg",
+      mobile: "./assets/images/image-macaron-mobile.jpg",
+      tablet: "./assets/images/image-macaron-tablet.jpg",
+      desktop: "./assets/images/image-macaron-desktop.jpg",
+    },
+    name: "Macaron Mix of Five",
+    category: "Macaron",
+    price: 8.0,
+  },
+  {
+    image: {
+      thumbnail: "./assets/images/image-tiramisu-thumbnail.jpg",
+      mobile: "./assets/images/image-tiramisu-mobile.jpg",
+      tablet: "./assets/images/image-tiramisu-tablet.jpg",
+      desktop: "./assets/images/image-tiramisu-desktop.jpg",
+    },
+    name: "Classic Tiramisu",
+    category: "Tiramisu",
+    price: 5.5,
+  },
+  {
+    image: {
+      thumbnail: "./assets/images/image-baklava-thumbnail.jpg",
+      mobile: "./assets/images/image-baklava-mobile.jpg",
+      tablet: "./assets/images/image-baklava-tablet.jpg",
+      desktop: "./assets/images/image-baklava-desktop.jpg",
+    },
+    name: "Pistachio Baklava",
+    category: "Baklava",
+    price: 4.0,
+  },
+  {
+    image: {
+      thumbnail: "./assets/images/image-meringue-thumbnail.jpg",
+      mobile: "./assets/images/image-meringue-mobile.jpg",
+      tablet: "./assets/images/image-meringue-tablet.jpg",
+      desktop: "./assets/images/image-meringue-desktop.jpg",
+    },
+    name: "Lemon Meringue Pie",
+    category: "Pie",
+    price: 5.0,
+  },
+  {
+    image: {
+      thumbnail: "./assets/images/image-cake-thumbnail.jpg",
+      mobile: "./assets/images/image-cake-mobile.jpg",
+      tablet: "./assets/images/image-cake-tablet.jpg",
+      desktop: "./assets/images/image-cake-desktop.jpg",
+    },
+    name: "Red Velvet Cake",
+    category: "Cake",
+    price: 4.5,
+  },
+  {
+    image: {
+      thumbnail: "./assets/images/image-brownie-thumbnail.jpg",
+      mobile: "./assets/images/image-brownie-mobile.jpg",
+      tablet: "./assets/images/image-brownie-tablet.jpg",
+      desktop: "./assets/images/image-brownie-desktop.jpg",
+    },
+    name: "Salted Caramel Brownie",
+    category: "Brownie",
+    price: 4.5,
+  },
+  {
+    image: {
+      thumbnail: "./assets/images/image-panna-cotta-thumbnail.jpg",
+      mobile: "./assets/images/image-panna-cotta-mobile.jpg",
+      tablet: "./assets/images/image-panna-cotta-tablet.jpg",
+      desktop: "./assets/images/image-panna-cotta-desktop.jpg",
+    },
+    name: "Vanilla Panna Cotta",
+    category: "Panna Cotta",
+    price: 6.5,
+  },
+  {
+    image: {
+      thumbnail:
+        "https://images.unsplash.com/photo-1725795351238-67b95f3ace3d?q=80&w=3600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      mobile:
+        "https://images.unsplash.com/photo-1725795351238-67b95f3ace3d?q=80&w=3600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      tablet:
+        "https://images.unsplash.com/photo-1725795351238-67b95f3ace3d?q=80&w=3600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      desktop:
+        "https://images.unsplash.com/photo-1725795351238-67b95f3ace3d?q=80&w=3600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    name: "New Product",
+    category: "New",
+    price: 1222233.5,
+  },
+];
+
+function showProduct(item) {
+  const clonedProduct = product.cloneNode(true); // deep clone should receive a true argument
+  console.log(clonedProduct.children[1].children);
+  const clonedSource0 = clonedProduct.children[0].children[0].children[0];
+
+  const clonedSource1 = clonedProduct.children[0].children[0].children[1];
+  const clonedImg = clonedProduct.children[0].children[0].children[2];
+  clonedImg.src = item.image.desktop;
+  clonedSource1.setAttribute("srcset", item.image.mobile);
+  clonedSource0.setAttribute("srcset", item.image.tablet);
+
+  const clonedCategory = clonedProduct.children[1].children[0];
+  const clonedTitle = clonedProduct.children[1].children[1];
+  const clonedPrice = clonedProduct.children[1].children[2];
+
+  clonedCategory.textContent = item.category;
+  clonedTitle.textContent = item.name;
+  clonedPrice.textContent = "$ " + item.price;
+  products.appendChild(clonedProduct);
+}
+
+productData.shift(); // remove the first member
+productData.forEach(showProduct);
